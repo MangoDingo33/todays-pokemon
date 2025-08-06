ad3f86723d52859fccc086ea358719a423b8c22b
MangoDingo33 <mangodingo33@gmail.com>
2025-08-06 10:57:43 +0900

    zoneinfo 라이브러리 이용 및 기타 수정

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# Pecharunt (ID: 1025)
+# Litten (ID: 725)
+**한국어 이름:** 냐오불
 
-**타입:** poison, ghost
+**타입:** fire
 
-**키:** 0.3 m
+**키:** 0.4 m
 
-**몸무게:** 0.3 kg
+**몸무게:** 4.3 kg
 
-**특성:** poison-puppeteer
+**특성:** blaze, intimidate
 
 ## 이미지
-- **[공식 일러스트]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1025.png)
-- **[도트]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1025.png)
-- **[이로치]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1025.png)
+- **[공식 일러스트]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/725.png)
+- **[도트]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/725.png)- **[이로치]** ![](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/725.png)
 
 
-⏳ 업데이트 시간: 2025-08-06 10:48:52 (KST)
+⏳ 업데이트 시간: 2025-08-06 10:57:12 (KST)
 
 ---
 자동 업데이트 봇에 의해 관리됩니다.
--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import random
 
 README_PATH = "README.md"
+
+def get_korean_name(pokemon_id):
+    """포켓몬 번호로 한국어 이름 추출"""
+    url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        for entry in data['names']:
+            if entry['language']['name'] == 'ko':
+                return entry['name']
+    return None
 
 def get_pokemon_info(pokemon_id):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}'
@@ -9,25 +22,27 @@ def get_pokemon_info(pokemon_id):
 
     if response.status_code == 200:
         data = response.json()
-        name = data['name'].title()
+        name_en = data['name'].title()
         id_ = data['id']
+        name_ko = get_korean_name(id_)
         types = ", ".join([t['type']['name'] for t in data['types']])
-        height = data['height'] / 10  # m 단위 변환
-        weight = data['weight'] / 10  # kg 단위 변환
+        height = data['height'] / 10  # m
+        weight = data['weight'] / 10  # kg
         abilities = ", ".join([a['ability']['name'] for a in data['abilities']])
         artwork_url = data['sprites']['other']['official-artwork']['front_default']
         dot_url = data['sprites']['front_default']
         shiny_url = data['sprites']['front_shiny']
 
         info = (
-            f"# {name} (ID: {id_})\n\n"
+            f"# {name_en} (ID: {id_})\n"
+            f"**한국어 이름:** {name_ko if name_ko else '정보 없음'}\n\n"
             f"**타입:** {types}\n\n"
             f"**키:** {height} m\n\n"
             f"**몸무게:** {weight} kg\n\n"
             f"**특성:** {abilities}\n\n"
             f"## 이미지\n"
             f"- **[공식 일러스트]** ![]({artwork_url})\n"
-            f"- **[도트]** ![]({dot_url})\n"
+            f"- **[도트]** ![]({dot_url})"
             f"- **[이로치]** ![]({shiny_url})\n"
         )
         return info
@@ -36,11 +51,10 @@ def get_pokemon_info(pokemon_id):
 
 def update_readme():
     """README.md 파일을 업데이트 (숫자 아이디로 포켓몬 정보)"""
-    pokemon_id = 1025  # 숫자 ID로 지정 (ditto)
+    pokemon_id = random.randint(1, 1025)  # 원하는 도감 번호로 변경
     pokemon_info = get_pokemon_info(pokemon_id)
 
-    now_utc = datetime.utcnow()
-    now_kst = now_utc + timedelta(hours=9)
+    now_kst = datetime.now(ZoneInfo("Asia/Seoul"))
     now_str = now_kst.strftime("%Y-%m-%d %H:%M:%S")
 
     readme_content = f"""{pokemon_info}
